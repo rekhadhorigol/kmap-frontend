0fcd68981dc58df36503e3202f5bf54418afc8cf
rekhadhorigol <rekhadhorigol@gmail.com>
2025-11-16 02:04:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "@/index.css";
-import App from "@/App";
+import "./index.css";
+import App from "./App";
 import { Toaster } from "sonner";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -9,5 +9,5 @@ root.render(
   <React.StrictMode>
     <App />
     <Toaster position="top-right" richColors />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
