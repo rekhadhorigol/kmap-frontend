cf92ea13d51f75306cf4cde15b240c276e64d5b9
rekhadhorigol <rekhadhorigol@gmail.com>
2025-11-16 03:00:02 +0530

    Update VerilogPanel.jsx

--- src/components/VerilogPanel.jsx
+++ src/components/VerilogPanel.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Card } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Card } from "../components/ui/card";
+import { Button } from "../components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../components/ui/tabs";
 import { Download, Copy } from "lucide-react";
 import { toast } from "sonner";
 import WaveformViewer from "./WaveformViewer";
