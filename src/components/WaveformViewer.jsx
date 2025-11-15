4931f28cb659c40cc026827e73515a60964033a1
rekhadhorigol <rekhadhorigol@gmail.com>
2025-11-16 03:00:18 +0530

    Update WaveformViewer.jsx

--- src/components/WaveformViewer.jsx
+++ src/components/WaveformViewer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card } from "@/components/ui/card";
+import { Card } from "../components/ui/card";
 
 export default function WaveformViewer({ waveformData, varNames, numVars }) {
   if (!waveformData || !waveformData.signals) {
