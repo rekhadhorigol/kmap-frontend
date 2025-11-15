5af3b26d6ee3188ebab70463cf3b2bf8ed97d5ab
rekhadhorigol <rekhadhorigol@gmail.com>
2025-11-16 02:57:59 +0530

    Update InputPanel.jsx

--- src/components/InputPanel.jsx
+++ src/components/InputPanel.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Card } from "../../components/ui/card";
-import { Button } from "../../components/ui/button";
-import { Label } from "../../components/ui/label";
-import { Input } from "../../components/ui/input";
-import { Textarea } from "../../components/ui/textarea";
-import { RadioGroup, RadioGroupItem } from "../../components/ui/radio-group";
+import { Card } from "../components/ui/card";
+import { Button } from "../components/ui/button";
+import { Label } from "../components/ui/label";
+import { Input } from "../components/ui/input";
+import { Textarea } from "../components/ui/textarea";
+import { RadioGroup, RadioGroupItem } from "../components/ui/radio-group";
 import { Calculator, Loader2 } from "lucide-react";
 
 export default function InputPanel({
